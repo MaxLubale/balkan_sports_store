a04f2d76133bf8e56e4f20ff0ffbf0407ced4dc9
Maxwell <lubalecliff@gmail.com>
2024-03-28 23:41:45 +0300

    created models for user,admin and products

---
+++ backend/models/admin.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  // Add more fields as needed
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = Admin;
